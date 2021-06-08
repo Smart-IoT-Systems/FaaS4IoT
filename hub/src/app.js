550b5d7ec4deff876c5dda917c6de3ac1d2a1ff2
nicolasferry <nicolas.ferry@univ-cotedazur.fr>
2021-06-08 13:55:58 +0200

    Self registration of the gateway in the broker

--- hub/src/app.js
+++ hub/src/app.js
@@ -2,16 +2,46 @@ const bridge = require('./ngsiv2.js');
 const express = require('express');
 const logger = require('./logger.js');
 const bodyParser = require('body-parser');
+var uuidv4 = require('uuid/v4');
 
 const app = express();
 var b;
 const urlOrion = process.env.ORION || "http://192.168.1.41:1026";
+const name = process.env.GW_NAME || "gateway-" + uuidv4();
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.listen(1212, '0.0.0.0', async function () {
     logger.log('info', 'Engine API started on 1212');
+
+    /**
+     * The gateway register itself in Orion
+     */
+    var ctxObj = {
+        "id": name,
+        "type": "gateway",
+        "equipped": {
+            "value": true
+        },
+        "location": {
+            "type": "GeoProperty",
+            "value": {
+                "type": "Point",
+                "coordinates": [
+                    35.629021,
+                    140.156059
+                ]
+            }
+        }
+    };
+    let b2 = new bridge(urlOrion, app, "", "");
+    b2.createEntity(ctxObj).then((response) => {
+        logger.log("info", "Gateway register in Orion" + JSON.stringify(response));
+    }).catch((error) => {
+        logger.log("error", "Could not register gateway in Orion" + JSON.stringify(error));
+    });
+
 
     /**
      * Example:
