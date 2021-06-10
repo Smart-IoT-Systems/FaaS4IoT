c569ca0ee2396296a2734aaccda5c3f44b7b923e
nicolasferry <nicolas.ferry@univ-cotedazur.fr>
2021-06-10 16:08:15 +0200

    Small refactoring

--- deployer/server/src/engine.js
+++ deployer/server/src/engine.js
@@ -24,6 +24,83 @@ var engine = function () {
         that.FunctionRegistry.functions.push(func);
 
         // We deploy GeneSIS on the selected gateway if needed
+        await that.deployGenesis(idGateway);
+
+        // We prepare the software stack
+        await that.deploySoftwareStack(func);
+
+        // We add the function
+        let src = "";
+        if (!fs.existsSync(func.src)) {
+            src = func.src;
+        } else {
+            src = fs.readFileSync(func.src);
+        }
+        let archivePath = await that.archiveFunction(src);
+
+        // Need to retrieve the id of the newly created container
+        let conn_genesis = GeneSIS_Connector(urlOrion + ":8000");
+        let dm = await conn_genesis.loadFromGeneSISWithoutUI();
+        let container_id;
+        let docker_ip;
+        let docker_port;
+        let func_command;
+        for (let comp of dm.components) {
+            if (comp.function_host != undefined && comp.function_host) {
+                container_id = comp.container_id;
+                func_command = comp.properties[0].function_command;
+            }
+            if (comp._type === "/infra/docker_host") { // This assumes there is only one host in a runtime !
+                docker_ip = comp.ip;
+                docker_port = comp.port[0];
+            }
+        }
+
+        if (container_id !== undefined && docker_ip !== undefined && docker_port !== undefined) {
+            conn_genesis.uploadArchive({ ip: docker_ip, port: docker_port }, container_id, archivePath, "/");
+
+            // We start it
+            conn_genesis.executeCommand({ ip: docker_ip, port: docker_port }, container_id, {
+                Cmd: [func_command, "/function"],
+                AttachStdout: true,
+                AttachStderr: true,
+                Tty: true
+            });
+        } else {
+            await conn_genesis.deploy({});
+            logger.log("error", "Could not deploy runtime. Thereby, your function has not been deployed.");
+        }
+    }
+
+    /**
+     * Used to terminate a function
+     * @param {*} id ID of the function to be removed
+     */
+    that.removeFunction = function (id) {
+
+    };
+
+    /**
+     * Used to deploy a software stack via GeneSIS
+     * @param {*} func 
+     */
+    that.deploySoftwareStack = async function (func) {
+        let model = {};
+        if (fs.existsSync(func.runtime)) {
+            let tmp = fs.readFileSync(func.runtime);
+            model = JSON.parse(tmp);
+        } else {
+            model = func.runtime;
+        }
+        let conn_genesis = GeneSIS_Connector(urlOrion + ":8000");
+        await conn_genesis.deploy(model);
+    }
+
+    /**
+     * Used to deploy  GeneSIS on a gateway. GeneSIS will be used to deploy the function software stack
+     * @param {*} idGateway 
+     */
+    that.deployGenesis = async function (idGateway) {
         let conn_ngsi = new NGSI.Connection(urlOrion + ":1026");
         conn_ngsi.v2.getEntity(idGateway).then(
             async (response) => {
@@ -57,68 +134,7 @@ var engine = function () {
             }, (error) => {
                 logger.log('error', error);
             });
-
-        // We prepare the software stack
-        let conn_genesis = GeneSIS_Connector(urlOrion + ":8000");
-        let model = {};
-        if (fs.existsSync(func.runtime)) {
-            let tmp = fs.readFileSync(func.runtime);
-            model = JSON.parse(tmp);
-        } else {
-            model = func.runtime;
-        }
-        await conn_genesis.deploy(model);
-
-        // We add the function
-        let src = "";
-        if (!fs.existsSync(func.src)) {
-            src = func.src;
-        } else {
-            src = fs.readFileSync(func.src);
-        }
-        let archivePath = await that.archiveFunction(src);
-
-        // Need to retrieve the id of the newly created container
-        let dm = await conn_genesis.loadFromGeneSISWithoutUI();
-        let container_id;
-        let docker_ip;
-        let docker_port;
-        let func_command;
-        for (let comp of dm.components) {
-            if (comp.function_host != undefined && comp.function_host) {
-                container_id = comp.container_id;
-                func_command = comp.properties[0].function_command;
-            }
-            if (comp._type === "/infra/docker_host") { // This assumes there is only one host in a runtime !
-                docker_ip = comp.ip;
-                docker_port = comp.port[0];
-            }
-        }
-
-        if (container_id !== undefined && docker_ip !== undefined && docker_port !== undefined) {
-            conn_genesis.uploadArchive({ ip: docker_ip, port: docker_port }, container_id, archivePath, "/"); // Fix docker_host and container_id
-
-            // We start it
-            conn_genesis.executeCommand({ ip: docker_ip, port: docker_port }, container_id, {
-                Cmd: [func_command, "/function"],
-                AttachStdout: true,
-                AttachStderr: true,
-                Tty: true
-            });
-        } else {
-            await conn_genesis.deploy({});
-            logger.log("error", "Could not deploy runtime. Thereby, your function has not been deployed.");
-        }
     }
-
-    /**
-     * Used to terminate a function
-     * @param {*} id ID of the function to be removed
-     */
-    that.removeFunction = function (id) {
-
-    };
-
 
 
     that.archiveFunction = async function (func, options = {}) {
