634a5b3c32e2bc354db797acc88410c349d010bb
Najar Ines <47181226+InesNajjar@users.noreply.github.com>
2021-08-30 12:50:45 +0100

    Update main.py

--- proxy/template/main.py
+++ proxy/template/main.py
@@ -7,15 +7,13 @@ host_name = "192.168.43.247"
 broker_port = 1883
 port_serial = "COM8"
 baudrate = 9600
-topic="serial/sub/arduino"
+topic="serial/temp"
 
 client=data_transfer.configConnection(host_name,broker_port)
 while True:
 	communication_device.serialcommunication(port_serial,baudrate)
 	serial_data = communication_device.serialcommunication(port_serial,baudrate)
-	#print("data received")
-	#print(serial_data)
+
 	formatted_data = data_structure.format_dataCSV_toNGSI(serial_data)
-	#print("data formatted")
+
 	data_transfer.publish(client,topic,formatted_data)
-	#print("data published")
