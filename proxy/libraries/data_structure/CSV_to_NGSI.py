2a1499e579998660cd01c91629bcc1a35b295bca
Najar Ines <47181226+InesNajjar@users.noreply.github.com>
2021-09-07 09:31:34 +0100

    Add files via upload

---
+++ proxy/libraries/data_structure/CSV_to_NGSI.py
@@ -0,0 +1,31 @@
+import json
+
+
+def format_dataCSV_toNGSI(dataToFormat):
+    entityId=""
+    entityType=""
+    datatype=""
+    datavalue=""
+    jsondata=""
+    entityId = dataToFormat.split(',')[0]
+    entityType = dataToFormat.split(',')[1]
+    datatype= dataToFormat.split(',')[2]
+    datavalue = dataToFormat.split(',')[3]
+    jsondata =json.dumps({
+                "contextElements": [
+                    {
+                        "entityId": {
+                            "id": entityId,
+                            "type": entityType
+                            },
+                        "attributes": [
+                              {
+                                "name": datatype,
+                                "type": "int",
+                                "value": datavalue
+                            
+                                }
+                            ]
+                    }
+                ]})
+    return jsondata
