93c3e7ac3bce972ce58994d4fe0b2c3a85a50a59
Najar Ines <47181226+InesNajjar@users.noreply.github.com>
2021-09-14 08:21:07 +0100

    Add files via upload

---
+++ proxy/examples/standardized/arduino_csv_data.ino
@@ -0,0 +1,21 @@
+// declare three Strings:
+
+String stringOne, stringTwo, stringThree;
+
+void setup() {
+  Serial.begin(9600);
+  stringTwo = String("Temperature,");
+  stringOne = String("Device.01,ArduinoUno,");
+  stringThree = String();
+}
+
+void loop() {
+int i =5;
+while(true){
+  stringThree =  stringOne + stringTwo + i;
+  Serial.println(stringThree); 
+  delay(1000);
+}
+  
+//  while (true);
+}
