26584a8e75ef0fa144ac8d44683b575f4ff27c4c
Najar Ines <47181226+InesNajjar@users.noreply.github.com>
2021-09-16 20:37:08 +0100

    Update and rename build.sh to build

--- proxy/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=$PYTHONPATH:"$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/libraries/Communication
-export PYTHONPATH=$PYTHONPATH:"$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/libraries/data_structure
-export PYTHONPATH=$PYTHONPATH:"$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/libraries/data_transfer
