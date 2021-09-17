1212aae873616b6dcafaec34c3f1fcfc887c681a
Najar Ines <47181226+InesNajjar@users.noreply.github.com>
2021-09-17 09:22:07 +0100

    Update main.py

--- proxy/template/main.py
+++ proxy/template/main.py
@@ -3,17 +3,17 @@ import CSV_to_NGSI
 import mqtt_transfer
 
 #Change these variables according to your environment
-host_name = "192.168.43.247"
+host_name = "192.168.43.77"
 broker_port = 1883
-port_serial = "/dev/ttyACM0"
+port_serial = "COM8"
 baudrate = 9600
 topic="serial/temp"
 
 client=data_transfer.configConnection(host_name,broker_port)
 while True:
-	communication_device.serialcommunication(port_serial,baudrate)
-	serial_data = communication_device.serialcommunication(port_serial,baudrate)
+	serial_comm.serialcommunication(port_serial,baudrate)
+	serial_data = serial_comm.serialcommunication(port_serial,baudrate)
 
-	formatted_data = data_structure.format_dataCSV_toNGSI(serial_data)
+	formatted_data = CSV_to_NGSI.format_dataCSV_toNGSI(serial_data)
 
-	data_transfer.publish(client,topic,formatted_data)
+	mqtt_transfer.publish(client,topic,formatted_data)
