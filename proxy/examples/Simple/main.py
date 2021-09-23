b8214f5139eaa7d768d61040b393ea07bae59bcc
Najar Ines <47181226+InesNajjar@users.noreply.github.com>
2021-09-23 10:19:43 +0100

    Update main.py

--- proxy/examples/Simple/main.py
+++ proxy/examples/Simple/main.py
@@ -1,5 +1,6 @@
-import communication_device
-import data_transfer
+import serial_comm
+import mqtt_transfer
+
 
 #Change these variables according to your environment
 host_name = "192.168.43.247"
@@ -8,11 +9,10 @@ port_serial = "/dev/ttyACM0"
 baudrate = 9600
 topic="serial/sub/arduino"
 
-client=data_transfer.configConnection(host_name,broker_port)
+client=mqtt_transfer.configConnection(host_name,broker_port)
 while True:
 	#get serial data
-	communication_device.serialcommunication(port_serial,baudrate)
-	serial_data = communication_device.serialcommunication(port_serial,baudrate)
-
+	serial_comm.serialcommunication(port_serial,baudrate)
+	serial_data = serial_comm.serialcommunication(port_serial,baudrate)
 	#send the data to Cloud broker
-	data_transfer.publish(client,topic,serial_data)
+	mqtt_transfer.publish(client,topic,serial_data)
