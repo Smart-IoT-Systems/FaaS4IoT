f2cf7f04016c7c9624c75b4745366b13fc672489
Najar Ines <47181226+InesNajjar@users.noreply.github.com>
2021-09-23 10:18:17 +0100

    Update main.py

--- proxy/examples/standardized/main.py
+++ proxy/examples/standardized/main.py
@@ -1,20 +1,20 @@
-import communication_device
-import data_structure
-import data_transfer
+import serial_comm
+import CSV_to_NGSI
+import mqtt_transfer
 
 #Change these variables according to your environment
-host_name = "192.168.43.247"
+host_name = "192.168.43.77"
 broker_port = 1883
 port_serial = "/dev/ttyACM0"
 baudrate = 9600
 topic="serial/sub/arduino"
 
-client=data_transfer.configConnection(host_name,broker_port)
+client=mqtt_transfer.configConnection(host_name,broker_port)
 while True:
 	#get serial data
-	communication_device.serialcommunication(port_serial,baudrate)
-	serial_data = communication_device.serialcommunication(port_serial,baudrate)
+	serial_comm.serialcommunication(port_serial,baudrate)
+	serial_data = serial_comm.serialcommunication(port_serial,baudrate)
 	#convert csv data to NGSI
-	formatted_data = data_structure.format_dataCSV_toNGSI(serial_data)
+	formatted_data = CSV_to_NGSI.format_dataCSV_toNGSI(serial_data)
 	#send the data to Cloud broker
-	data_transfer.publish(client,topic,formatted_data)
+	mqtt_transfer.publish(client,topic,formatted_data)
